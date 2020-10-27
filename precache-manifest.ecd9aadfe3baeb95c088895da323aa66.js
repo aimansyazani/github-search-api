982819e2de0fed26b80b122bbfc732d3d9498012
Aiman Syazani <aimansyazani@gmail.com>
2020-10-27 10:08:59 +0800

    Updates

---
+++ precache-manifest.ecd9aadfe3baeb95c088895da323aa66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ab17ab781367ec8c3d3ec03882fcb2",
+    "url": "/github-search-api/index.html"
+  },
+  {
+    "revision": "da0f92fe5c7ea8e044a3",
+    "url": "/github-search-api/static/css/main.c9076e39.chunk.css"
+  },
+  {
+    "revision": "eeb4d0f1dedde9dd2529",
+    "url": "/github-search-api/static/js/2.4b385848.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/github-search-api/static/js/2.4b385848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0f92fe5c7ea8e044a3",
+    "url": "/github-search-api/static/js/main.d2524ca3.chunk.js"
+  },
+  {
+    "revision": "1f39cd2632e4e071e206",
+    "url": "/github-search-api/static/js/runtime-main.5f62d8e5.js"
+  }
+]);
